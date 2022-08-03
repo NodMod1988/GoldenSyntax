afc2f64d2e11b210090f798f816ad13599d270d3
nodmod <niewerth.ruben@gmail.com>
2022-08-03 14:57:18 +0200

    Dmg created in Enemy class. closes #6

--- src/main/kotlin/Enemies.kt
+++ src/main/kotlin/Enemies.kt
@@ -2,6 +2,7 @@ open class Enemies{
 
     private var name: String = ""
     private var hp: Double = 0.0
+    private var dmg: Double = 0.0
 
     fun getHp(): Double{
         return hp
@@ -19,8 +20,17 @@ open class Enemies{
         this.name = name
     }
 
+    fun getDmg(): Double{
+        return dmg
+    }
+
+    fun setDmg(dmg: Double){
+        this.dmg = dmg
+    }
+
     constructor(name: String, hp: Double){
         setName(name)
         setHp(hp)
+        setDmg(dmg)
     }
 }
