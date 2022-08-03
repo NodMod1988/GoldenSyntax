792d7124086dce8b9ac88dc959f231c36899c5b6
nodmod <niewerth.ruben@gmail.com>
2022-08-03 13:10:07 +0200

    Create Getter/Setter for Enemy/Hero Class. closes #4

--- src/main/kotlin/Enemies.kt
+++ src/main/kotlin/Enemies.kt
@@ -1,2 +1,26 @@
-open class Enemies(name: String, hp: Double) {
+open class Enemies{
+
+    private var name: String = ""
+    private var hp: Double = 0.0
+
+    fun getGp(): Double{
+        return hp
+    }
+
+    fun setHp(hp:Double){
+        this.hp = hp
+    }
+
+    fun getName(): String{
+        return name
+    }
+
+    fun setName(name: String){
+        this.name = name
+    }
+
+    constructor(name: String, hp: Double){
+        setName(name)
+        setHp(hp)
+    }
 }
--- src/main/kotlin/Heroes.kt
+++ src/main/kotlin/Heroes.kt
@@ -1,3 +1,28 @@
-open class Heroes(name: String, hp: Double) {
+import java.util.DoubleSummaryStatistics
 
+open class Heroes{
+
+    private var name: String = ""
+    private var hp: Double = 0.0
+
+    fun getGp(): Double{
+        return hp
+    }
+
+    fun setHp(hp:Double){
+        this.hp = hp
+    }
+
+    fun getName(): String{
+        return name
+    }
+
+    fun setName(name: String){
+        this.name = name
+    }
+
+    constructor(name: String, hp: Double){
+        setName(name)
+        setHp(hp)
+    }
 }
