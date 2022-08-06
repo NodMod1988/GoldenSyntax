3dc479baadd80a3e0b21abc8ee2836229c16263b
NodMod1988 <niewerth.ruben@gmail.com>
2022-08-06 12:31:06 +0200

    created healer class

---
+++ src/main/kotlin/Healer.kt
@@ -0,0 +1,13 @@
+class Healer(name:String, hp: Double, dmg: Double) : Heroes(name,hp,dmg) {
+
+
+    override fun attack(enemy: Enemies): Enemies {
+        enemy.setHp(enemy.getHp()-getDmg())
+        return enemy
+    }
+
+    fun heal(heroes: Heroes): Heroes {
+        heroes.setHp(heroes.getHp() + 30)
+        return heroes
+    }
+}
